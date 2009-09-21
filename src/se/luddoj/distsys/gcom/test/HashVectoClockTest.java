b08734b99b59ddcf0c1b5b03957bca304e30dc0c
Emil Eriksson <c07een@cs.umu.se>
2009-09-21 14:10:42 +0200

    Starting on the vectorclock

---
+++ src/se/luddoj/distsys/gcom/test/HashVectoClockTest.java
@@ -0,0 +1,19 @@
+package se.luddoj.distsys.gcom.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class HashVectoClockTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testCompareToHashVectorClock() {
+		fail("Not yet implemented");
+	}
+
+}
