9701f48bedab698cf445fc68e1f9b311b9e0c71f
Emil Eriksson <asheidan@gmail.com>
2009-09-22 12:02:42 +0200

    Small change in CommunicationModule, refactoring to CommunicationModuleInterface imminent.

--- src/se/luddoj/distsys/gcom/CommunicationModule.java
+++ src/se/luddoj/distsys/gcom/CommunicationModule.java
@@ -3,5 +3,5 @@ package se.luddoj.distsys.gcom;
 public interface CommunicationModule {
 	public void receive(MessageInterface m);
 	
-	public void send(String groupName, MessageInterface m);
+	public void send(MessageInterface m);
 }
