eaa28d64be92e6e15f81223e8a675211cad7a55d
Emil Eriksson <asheidan@gmail.com>
2009-09-23 20:31:16 +0200

    Fixes after merging with old version.

--- src/gcom/GCom.java
+++ src/gcom/GCom.java
@@ -1,11 +1,20 @@
 package gcom;
 
+import gcom.interfaces.CommunicationModule;
+import gcom.interfaces.GroupManagementModule;
+import gcom.interfaces.MessageListener;
+import gcom.interfaces.ViewChangeListener;
+import gcom.interfaces.GroupDefinition;
+import gcom.interfaces.Member;
+
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.List;
 
-public class GCom implements GComInterface {
+public class GCom implements gcom.interfaces.GCom {
 
+	private GroupManagementModule gmm = new gcom.GroupManagementModule();
+	
 	@Override
 	public void addMessageListener(String groupName, MessageListener listener) {
 		// TODO Auto-generated method stub
@@ -26,10 +35,9 @@ public class GCom implements GComInterface {
 	}
 
 	@Override
-	public void createGroup(GroupDefinitionInterface description,
+	public void createGroup(GroupDefinition description,
 			String localMemberName) throws IOException {
 		// TODO Auto-generated method stub
-
 	}
 
 	@Override
@@ -39,26 +47,25 @@ public class GCom implements GComInterface {
 	}
 
 	@Override
-	public MemberInterface getLocalMember(String groupName) {
+	public Member getLocalMember(String groupName) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public List<MemberInterface> getMembers(String groupName) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Member> getMembers(String groupName) {
+		return gmm.getGroup(groupName).listMembers();
 	}
 
 	@Override
-	public GroupDefinitionInterface joinGroup(String groupName,
+	public GroupDefinition joinGroup(String groupName,
 			String localMemberName) throws IOException, IllegalStateException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public List<GroupDefinitionInterface> listGroups() throws IOException {
+	public List<GroupDefinition> listGroups() throws IOException {
 		// TODO Auto-generated method stub
 		return null;
 	}
