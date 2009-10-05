48ba9101694d4388c7fda383dd77c9226b4cdbc3
ludw <ludw@zirc.(none)>
2009-10-05 17:11:41 +0200

    Causal message ordering implemented but needs more testing

--- src/gcom/HashVectorClock.java
+++ src/gcom/HashVectorClock.java
@@ -18,43 +18,55 @@ public class HashVectorClock implements VectorClock {
 
 	private Hashtable<Object, Integer> clocks;
 	private Object key;
-	
+
 	public HashVectorClock(Object key) {
 		clocks = new Hashtable<Object, Integer>();
 		this.key = key;
 		clocks.put(key, 0);
 	}
-	
+
 	public HashVectorClock(Object key, Map<Object, Integer> m) {
 		clocks = new Hashtable<Object, Integer>(m);
 		this.key = key;
 	}
-	
+
 	public void tick() {
 		clocks.put(key, clocks.get(key)+1);
 	}
-	
+
+	public void tickKey(Object k) {
+		if(clocks.get(k) == null) {
+			clocks.put(k, 1);
+			return;
+		}
+		clocks.put(k, clocks.get(k)+1);
+	}
+
 	public void merge(HashVectorClock o) {
-		logger.debug(String.format("Merging %s with %s", toString(), o.toString()));
+		//logger.debug(String.format("Merging %s with %s", toString(), o.toString()));
 		for(Object key : o.clocks.keySet()) {
 			Integer my_value = clocks.get(key);
 			Integer other_value = o.clocks.get(key);
+			if(my_value == null) {
+				clocks.put(key, other_value);
+				continue;
+			}
 			if(my_value < other_value) {
 				clocks.put(key, other_value);
 			}
 		}
 	}
-	
+
 	public int compareTo(HashVectorClock o) {
-		logger.debug(String.format("Comparing %s with %s", toString(), o.toString()));
+		//logger.debug(String.format("Comparing %s with %s", toString(), o.toString()));
 		int later = 0;
 		int earlier = 0;
 		for(Object key : clocks.keySet()){
 			Integer otherClockValue = o.clocks.get(key);
-			if(otherClockValue != null) {
+			if(otherClockValue != null) { 
 				switch(clocks.get(key).compareTo(otherClockValue)) {
-				case -1: earlier = -1; break;
-				case 1: later = 1; break;
+					case -1: earlier = -1; break;
+					case 1: later = 1; break;
 				}
 			}
 		}
@@ -64,13 +76,14 @@ public class HashVectorClock implements VectorClock {
 	public int compareTo(VectorClock o) {
 		throw(new RuntimeException("Method not implemented: compareTo(VectorClock)"));
 	}
-	
+
 	public String toString() {
 		return "VectorClock(" +this.key.toString() + ")"+ clocks.toString(); 
 	}
 
 
 	public Integer getValue(Object key) {
+		if(clocks.get(key) == null) return 0;
 		return clocks.get(key);
 	}
 
