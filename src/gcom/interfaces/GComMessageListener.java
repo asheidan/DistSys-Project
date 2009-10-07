bd99537e3a97417b65f0de8dc6c742116f9320be
Emil Eriksson <asheidan@gmail.com>
2009-10-07 15:47:53 +0200

    Added an interface

---
+++ src/gcom/interfaces/GComMessageListener.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gcom.interfaces;
+
+/**
+ *
+ * @author emil
+ */
+public interface GComMessageListener {
+	public void messageReceived(Message message);
+}
