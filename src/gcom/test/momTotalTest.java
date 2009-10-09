66d0f831fec39be4f9dd1881b575ecb304c93716
Ludvig Widman <dit06lwn@oceankabel.cs.umu.se>
2009-10-09 10:16:48 +0200

    Total test

--- src/gcom/test/momTotalTest.java
+++ src/gcom/test/momTotalTest.java
@@ -42,6 +42,31 @@ public class momTotalTest {
 		assertEquals(m2, listener.recieved.get(1));
 		assertEquals(m3, listener.recieved.get(2));
 	}
+	
+	@Test
+	public void testTwoSources() {
+		Member source1 = new Member("1", "test1");
+		Member source2 = new Member("2", "test2");
+		
+		testMessageListener listener = new testMessageListener();
+		mom.addMessageListener(listener);
+
+		Message m0 = getMessage(source1, 0);
+		Message m1 = getMessage(source2, 1);
+		Message m2 = getMessage(source1, 2);
+		Message m3 = getMessage(source2, 3);
+
+		mom.queueMessage(m0);
+		mom.queueMessage(m1);
+		mom.queueMessage(m2);
+		mom.queueMessage(m3);
+
+		assertEquals("All messages recieved", 4, listener.recieved.size());
+		assertEquals(m0, listener.recieved.get(0));
+		assertEquals(m1, listener.recieved.get(1));
+		assertEquals(m2, listener.recieved.get(2));
+		assertEquals(m3, listener.recieved.get(3));
+	}
 
 	private Message getMessage(Member source, int ticks) {
 		HashVectorClock clock = new HashVectorClock("serialNo");
