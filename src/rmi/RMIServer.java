da11c590153e3b1ba34750c067a795929dc6d244
Emil Eriksson <asheidan@gmail.com>
2009-10-11 04:10:50 +0200

    Added a simple class for launching a registry. Registry shuts down when button is pressed.

---
+++ src/rmi/RMIServer.java
@@ -0,0 +1,30 @@
+package rmi;
+
+import gcom.Debug;
+import java.rmi.RemoteException;
+import java.rmi.registry.*;
+import javax.swing.JOptionPane;
+
+public class RMIServer {
+	Registry registry;
+	public static void main(String [] args) {
+		
+		try {
+			new RMIServer(Integer.valueOf(args[0]));
+			JOptionPane.showMessageDialog(null,"RMIRegistry is launched\nListening on port " + args[0],"RMI:" + args[0],JOptionPane.INFORMATION_MESSAGE);
+		}
+		catch(ArrayIndexOutOfBoundsException e) {
+			Debug.log("RMIServer",Debug.ERROR, "You must supply a portnumber");
+		}
+		System.exit(0);
+	}
+	
+	public RMIServer(int port) {
+		try {
+			registry = LocateRegistry.createRegistry(port);
+		}
+		catch(RemoteException e) {
+			Debug.log("RMIServer",Debug.ERROR, "Got remote exception", e);
+		}
+	}
+}
